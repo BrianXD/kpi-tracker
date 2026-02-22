cd3d579202f6b23755b58d390bf44459a34e6881
Brian Lin <cylin@gmail.com>
2026-02-22 22:41:44 +0800

    fix(router): switch to HashRouter for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
 import WorkItemFormPage from './pages/WorkItemFormPage'
 import AdminPage from './pages/AdminPage'
@@ -7,7 +7,7 @@ import DashboardPage from './pages/DashboardPage'
 
 export default function App() {
   return (
-    <BrowserRouter basename="/kpi-tracker">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/form" element={<WorkItemFormPage />} />
@@ -16,7 +16,7 @@ export default function App() {
         <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
